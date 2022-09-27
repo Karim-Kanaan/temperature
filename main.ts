92dc2076501ae33a89e5e3ef14097f3f711a27dc
Karim-Kanaan <113034405+Karim-Kanaan@users.noreply.github.com>
2022-09-27 09:21:19 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let current_temperature = 0
 // In the forever block so that it is constantly running wqithout the need for human intervention
 basic.forever(function () {
-    // So that the variable is in celsius formayt
+    // So that the variable is in celsius format
     current_temperature = input.temperature()
     basic.showNumber(current_temperature)
     basic.pause(1000)
