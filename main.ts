7c02c2784131e348a360434097c7e3dfb1847d7c
Karim-Kanaan <113034405+Karim-Kanaan@users.noreply.github.com>
2022-09-27 09:20:10 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let current_temperature = 0
+// In the forever block so that it is constantly running wqithout the need for human intervention
+basic.forever(function () {
+    // So that the variable is in celsius formayt
+    current_temperature = input.temperature()
+    basic.showNumber(current_temperature)
+    basic.pause(1000)
+    basic.clearScreen()
+})
