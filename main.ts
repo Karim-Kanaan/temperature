0187d0c724068fa7fe8065b699d8e2e9a52d54c3
Karim-Kanaan <113034405+Karim-Kanaan@users.noreply.github.com>
2022-09-27 09:25:49 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 let current_temperature = 0
-// In the forever block so that it is constantly running wqithout the need for human intervention
+// Created by: Karim Kanaan
+// Created on: Sept 27 2022
+// 
+// This program tells the microbit to display the value of the temperature variable
 basic.forever(function () {
-    // So that the variable is in celsius format
     current_temperature = input.temperature()
     basic.showNumber(current_temperature)
     basic.pause(1000)
